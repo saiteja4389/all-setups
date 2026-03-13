c2a40720374897515b656d981b47121eeb53ebe1
saiteja4389 <81801580+saiteja4389@users.noreply.github.com>
2026-03-13 19:47:27 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -2,8 +2,8 @@ sudo yum update -y
 sudo yum install wget -y
 sudo yum install java-17-amazon-corretto-jmods -y
 sudo mkdir /app && cd /app
-sudo wget https://download.sonatype.com/nexus/3/nexus-3.79.1-04-linux-x86_64.tar.gz
-sudo tar -xvf nexus-3.79.1-04-linux-x86_64.tar.gz
+sudo wget https://download.sonatype.com/nexus/3/nexus-3.90.1-01-linux-x86_64.tar.gz
+sudo tar -xvf nexus-3.90.1-01-linux-x86_64.tar.gz
 sudo mv nexus-3.79.1-04 nexus
 sudo adduser nexus
 sudo chown -R nexus:nexus /app/nexus
